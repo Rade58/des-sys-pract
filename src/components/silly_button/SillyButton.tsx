a5a8c4143b52bc5407b7ee31d077ecf0f2801870
rade <bajic.rade2@gmail.com>
2025-11-08 21:19:09 +0100

    clsx

--- src/components/silly_button/SillyButton.tsx
+++ src/components/silly_button/SillyButton.tsx
@@ -9,8 +9,10 @@ export type SillyButtonProps = ComponentProps<"button"> & {
 };
 
 export function SillyButton({ variant, ...props }: SillyButtonProps) {
+  // without clsx library
+  /*
+  
   let className: string = styles.button;
-
   switch (variant) {
     case "primary":
       break;
@@ -22,7 +24,40 @@ export function SillyButton({ variant, ...props }: SillyButtonProps) {
       break;
     default:
       break;
-  }
-
+  } 
+      
   return <button className={className} {...props} />;
+  */
+  //  with libray
+
+  return (
+    <button
+      {...props}
+      /* className={clsx(
+        styles.button,
+
+        variant === "secondary" && styles.secondary,
+        variant === "destructive" && styles.destructive,
+        variant === "primary" && false, // I added this but it is unnecessary
+      )} */
+
+      // we can do it like this because
+      // we want actual string of the our scoped css class
+      // or we want undefined in order to not apply class
+      // so styles[variant] can give a string or undefined
+      // only thing is that I hate this, and also
+      // typscript hates this
+      // className={clsx(styles.button, styles[variant])}
+
+      // favorite way
+
+      className={clsx(styles.button, {
+        [styles.primary]: false, // added this jus to show you that
+        // in terms of our css file we don't have .primary class
+        // .button is our primary button style
+        [styles.secondary]: variant === "secondary",
+        [styles.destructive]: variant === "destructive",
+      })}
+    />
+  );
 }
